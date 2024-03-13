848cd968ee5ceaefbf5196172f7aef99d7a9ae8d
Aleksei Melnik <alexshnup@gmail.com>
2024-03-13 13:34:06 -0400

    fixed Duplicate text + html and image Attachments Allowed

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,9 @@ import boto3
 import email
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+from email import encoders
 
 s3 = boto3.client('s3')
 ses = boto3.client('ses')
@@ -32,17 +35,40 @@ def lambda_handler(event, context):
         forward_source = record['ses']['mail']['source'] # The original sender
         forward_msg['Subject'] = "Fwd: "+ forward_source + " " + msg['Subject']
         
+        no_need_text_plain = False
         # Check if the message is multipart
         if msg.is_multipart():
+            
             for part in msg.walk():
-                # Only process text/plain or text/html parts
-                if part.get_content_type() in ['text/plain', 'text/html']:
+                if part.get_content_type() in ['text/html']:
+                    no_need_text_plain = True
+            for part in msg.walk():
+                print("Processing part, ContentType=", part.get_content_type())
+                if part.get_content_type() in ['text/html']:
                     part_payload = part.get_payload(decode=True)  # Decode binary string
                     charset = part.get_content_charset() or 'utf-8'  # Default charset to utf-8 if not specified
                     # Correctly create the MIMEText part
                     mime_part = MIMEText(part_payload.decode(charset), part.get_content_subtype(), charset)
                     forward_msg.attach(mime_part)
+                # Skip container multipart types
+                if part.get_content_maintype() == 'multipart':
+                    continue
+                elif not no_need_text_plain and part.get_content_maintype() == 'text':
+                    # Handle text/plain and text/html
+                    forward_msg.attach(MIMEText(part.get_payload(decode=True), part.get_content_subtype(), part.get_content_charset()))
+                elif part.get_content_maintype() == 'image':
+                    # Handle image attachments
+                    img = MIMEImage(part.get_payload(decode=True), part.get_content_subtype())
+                    img.add_header('Content-Disposition', 'attachment', filename=part.get_filename())
+                    forward_msg.attach(img)
+                elif part.get_content_maintype() == 'application':
+                    # Handle generic application/octet-stream types and others
+                    app = MIMEApplication(part.get_payload(decode=True), part.get_content_subtype())
+                    app.add_header('Content-Disposition', 'attachment', filename=part.get_filename())
+                    encoders.encode_base64(app)
+                    forward_msg.attach(app)
         else:
+            print("Procession as a not multipart")
             # For non-multipart messages, decode and attach as plain text
             text_payload = msg.get_payload(decode=True)
             charset = msg.get_content_charset() or 'utf-8'
